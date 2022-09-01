710cda5eae111e808a0c4d82a0351fba45416725
ashiikkhan <ashiksust06@gmail.com>
2022-09-02 00:55:24 +0600

    creating syntax

--- object.js
+++ object.js
@@ -1,2 +1,47 @@
 //create object using object literals:
-const player = {};
+const musician = {};
+musician.name = 'MD. ASHIKUR RAHMAN';
+musician.speciality = 'Guitar Player';
+musician.play = function () {
+  console.log('start to play your guitar');
+};
+
+// console.log(musician);
+//{ name: 'MD. ASHIKUR RAHMAN', speciality: 'Guitar Player' }
+
+//object constructor:
+const person = new Object();
+// console.log(person);
+
+//Object.create() method:
+const item = Object.create(musician);
+// console.log(item);
+//{}
+// console.log(item.name);
+// MD. ASHIKUR RAHMAN
+
+//ES6 Classes:
+class Person {
+  name = 'Ashik';
+  age;
+  job;
+  address = {
+    area: 'Sonali R/A, Notun Bazar',
+    district: 'Sylhet',
+  };
+  constructor(age, job) {
+    this.age = age;
+    this.job = job;
+  }
+}
+
+const person1 = new Person(26, 'Web Developer');
+// console.log(person1);
+
+//Constructor function :
+function Car(model, price) {
+  this.model = model;
+  this.price = price;
+}
+const tesla = new Car('eloion', 50000);
+console.log(tesla);
